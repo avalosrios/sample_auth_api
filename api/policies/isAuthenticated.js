8787c5478b1e37b47dd129337e0b3aa5d06e9be0
Oswaldo Avalos <oavalos@agari.com>
2016-02-25 12:01:29 -0800

    Added error response code on authentication

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -6,26 +6,20 @@ module.exports = function(req, res, next) {
     function(err, user, info){
       console.log(user);
       if( (err) || (!user)){
-        res.set("WWW-Authenticate", "Basic realm=\"Restricted\"");
-        var message;
         if(err){
-          message = error.message;
+          return res.send({
+            error: err,
+            user: user
+          },500);
         }else{
-          message = "Not authenticated";
+          res.set("WWW-Authenticate", "Basic realm=\"Restricted\"");
+          return res.send({
+            message: "Not authenticated",
+            user: user
+          },401);
         }
-        return res.send({
-          message: message,
-          user: user
-        },401);
+
       }
       return next(null, user);
-      /*
-      req.logIn(user, function(err){
-        if(err) res.send(err);
-        return res.send({
-          message: info.message,
-          user: user
-        });
-      });*/
     })(req, res, next);
 };
